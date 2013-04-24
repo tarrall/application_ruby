0b6cdec7174f5adcd3467ac47783c36e19dab201
Robert Tarrall <robert.tarrall@sendgrid.com>
2013-04-24 10:58:10 -0600

    Support for environment_name attribute in rails provider

--- resources/rails.rb
+++ resources/rails.rb
@@ -29,6 +29,7 @@ attribute :bundler_deployment, :kind_of => [NilClass, TrueClass, FalseClass], :d
 attribute :bundler_without_groups, :kind_of => [Array], :default => []
 attribute :bundle_command, :kind_of => [String, NilClass], :default => "bundle"
 attribute :precompile_assets, :kind_of => [NilClass, TrueClass, FalseClass], :default => nil
+attribute :environment_name, :kind_of => [String, NilClass], :default => nil
 
 def database(*args, &block)
   @database ||= Mash.new
